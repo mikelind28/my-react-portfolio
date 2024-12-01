fb45ef2e0deae722451fb6d6454e0894479c9ebd
Mike Lind <mikelind28@gmail.com>
2024-12-01 17:57:39 -0600

    format error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,8 +6,8 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page">
-      <h1>Hmmm...</h1>
-      <p>An unexpected error has occurred.</p>
+      <h1>hmmm...</h1>
+      <p>an unexpected error has occurred:</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
