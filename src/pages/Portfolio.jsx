d6f6df465df9e1cfcdff6d1e6dd69a90fe4a08c5
Mike Lind <mikelind28@gmail.com>
2024-12-02 16:21:43 -0600

    add screenshots of apps

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,9 @@
-import mountain from '../assets/images/mountain.jpg';
-import forest from '../assets/images/forest.jpg';
-import prairie from '../assets/images/prairie.jpg';
-import rainforest from '../assets/images/rainforest.jpg';
-import river from '../assets/images/river.png';
+import weather from '../assets/images/weather-dashboard.png';
+import employee from '../assets/images/employee-tracker.png';
+import vehicle from '../assets/images/vehicle-builder.png';
+import task from '../assets/images/task-tracker.png';
+import readme from '../assets/images/readme-made-easy.png';
+import css from '../assets/images/css-testing.png';
 import Project from '../components/Project';
 
 export default function Portfolio() {
@@ -13,8 +14,8 @@ export default function Portfolio() {
 
           <Project
           id={"app-1"}
-          src={mountain}
-          alt={"mountain"}
+          src={weather}
+          alt={"weather-dashboard"}
           mainLink={"https://weather-dashboard-yntg.onrender.com/"} 
           mainLinkTitle={"weather-dashboard app"} 
           subLink={"https://github.com/mikelind28/weather-dashboard"} 
@@ -23,8 +24,8 @@ export default function Portfolio() {
 
           <Project
           id={"app-2"}
-          src={forest}
-          alt={"forest"}
+          src={employee}
+          alt={"employee-tracker"}
           mainLink={"https://github.com/mikelind28/employee-tracker"} 
           mainLinkTitle={"employee-tracker app"} 
           subLink={"https://drive.google.com/file/d/1DXPyMtcYDwTl7HKKeCx4kH0KibFfOCjW/view"} 
@@ -33,8 +34,8 @@ export default function Portfolio() {
 
           <Project
           id={"app-3"}
-          src={prairie}
-          alt={"prairie"}
+          src={vehicle}
+          alt={"vehicle-builder"}
           mainLink={"https://github.com/mikelind28/CLI-Vehicle-Builder"} 
           mainLinkTitle={"vehicle-builder app"} 
           subLink={"https://drive.google.com/file/d/13WOD0GomK0IZek2z0PmVfFD3Ou4KQqwj/view"} 
@@ -43,8 +44,8 @@ export default function Portfolio() {
 
           <Project
           id={"app-4"}
-          src={rainforest}
-          alt={"rainforest"}
+          src={task}
+          alt={"task-tracker"}
           mainLink={"https://toli-a.github.io/project1/"} 
           mainLinkTitle={"task-tracker app"} 
           subLink={"https://github.com/toli-A/project1"} 
@@ -53,8 +54,8 @@ export default function Portfolio() {
 
           <Project
           id={"app-5"}
-          src={river}
-          alt={"river"}
+          src={readme}
+          alt={"readme-made-easy"}
           mainLink={"https://github.com/mikelind28/README-Made-Easy"} 
           mainLinkTitle={"readme-made-easy app"} 
           subLink={"https://drive.google.com/file/d/1H47A6TUaJYyMTn4uYytp4EBFaR6e6e1T/view"} 
@@ -63,8 +64,8 @@ export default function Portfolio() {
 
           <Project
           id={"app-6"}
-          src={forest}
-          alt={"forest"}
+          src={css}
+          alt={"css-testing"}
           mainLink={"https://mikelind28.github.io/css-testing/"} 
           mainLinkTitle={"css-testing"} 
           subLink={"https://github.com/mikelind28/css-testing"} 
